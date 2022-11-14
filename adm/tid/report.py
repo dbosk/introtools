4c4289a2ce1d5e30adcd0d080750aa14fef84667
Daniel Bosk <dbosk@kth.se>
2022-11-14 10:31:34 +0100

    report.py: Fetches personnummer from LADOK

--- adm/tid/report.py
+++ adm/tid/report.py
@@ -1,19 +1,23 @@
-import arrow
+import canvasapi
+import canvaslms.cli
 import csv
-import nytid.schedules as sched
+import ladok3.kth
 import nytid.schedules.utils as utils
 import operator
+import os
 import sys
 
 def to_hours(td):
     return td.total_seconds()/60/60
 
 def summarize_user(user, course_events):
-    csvout = csv.writer(sys.stdout)
-
     hours = to_hours(utils.hours_per_TA(course_events)[user])
 
-    csvout.writerow([f"{user}@kth.se", "Total (inkl förberedelsetid): ",
+    student = ls.get_student(get_ladok_id(f"{user}@kth.se"))
+
+    csvout.writerow([student.personnummer, student.last_name, 
+                     student.first_name, f"{user}@kth.se"])
+    csvout.writerow(["", "Total (inkl förberedelsetid): ",
                      f"{round(hours, 2)} h", f"{round(hours*150)} kr"])
 
     start_idx = utils.SIGNUP_SHEET_HEADER.index("Start")
@@ -38,6 +42,16 @@ def summarize_user(user, course_events):
     csvout.writerow([])
     csvout.writerow([])
 
+def get_ladok_id(user):
+    for student in students:
+        if student.login_id == user:
+            return student.integration_id
+
+cs = canvasapi.Canvas(os.environ["CANVAS_SERVER"], os.environ["CANVAS_TOKEN"])
+course = next(canvaslms.cli.courses.filter_courses(cs, "datintro22"))
+students = list(course.get_users())
+
+ls = ladok3.kth.LadokSession(os.environ["KTH_LOGIN"], os.environ["KTH_PASSWD"])
 
 SIGNUP = {
         "DD1301": utils.google_sheet_to_csv_url(
@@ -49,7 +63,7 @@ datintro = []
 for course, url in SIGNUP.items():
     datintro += utils.read_signup_sheet_from_url(url)
 
-csvout = csv.writer(sys.stdout)
+csvout = csv.writer(sys.stdout, delimiter="\t")
 csvout.writerow(["Tidrapport"])
 csvout.writerow([])
 
